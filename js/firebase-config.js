8bb6b2e042f92ebc056d94b1335e3dab91378dd4
Tomate <louis44.boucard@gmail.com>
2026-05-25 22:56:14 +0200

    feat: update firebase configuration to use environment variables

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1reaUvJD39XEPVv1bBBB9CD_icc1CX7A",
-  authDomain: "wishlist-907be.firebaseapp.com",
-  projectId: "wishlist-907be",
-  storageBucket: "wishlist-907be.firebasestorage.app",
-  messagingSenderId: "961573518397",
-  appId: "1:961573518397:web:7cb1d2ea0bf7c3564d3e54",
-  measurementId: "G-C2Z97D94X2"
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
